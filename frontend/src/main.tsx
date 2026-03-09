780fed3f55813c3412af00a4e331c697b5cbe295
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-03-09 14:46:25 -0400

    adding offline message

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+
+import Offline from "./Offline";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Offline />
   </StrictMode>,
 );
