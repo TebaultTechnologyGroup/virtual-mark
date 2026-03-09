3ae65e55fea104e51db6822e37d1d99b2318922c
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-03-09 14:46:39 -0400

    offline message

---
+++ frontend/src/Offline.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Offline.css";
+
+const Offline: React.FC = () => {
+  return (
+    <div className="offline-container">
+      <h1>Virtual Mark is Temporarily Offline</h1>
+      <p>I apologize for the inconvenience. Please check back soon.</p>
+    </div>
+  );
+};
+
+export default Offline;
