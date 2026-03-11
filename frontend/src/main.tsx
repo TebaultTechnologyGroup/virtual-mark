13348ab7913aeae71bf637d5050a0d0b4deeef10
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-03-11 11:18:40 -0400

    Removed offline message

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import Offline from "./Offline";
+//import Offline from "./Offline";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Offline />
+    <App />
   </StrictMode>,
 );
